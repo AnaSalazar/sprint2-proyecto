3136c5edce4ca0d4f1bd2d1d8c470ffbec0f6a19
AnaSalazar <anastephany.salazarguzman@gmail.com>
2017-03-29 13:06:17 -0600

    solucionado problema de elemento de lista vacio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-function crearLista() {
+function Lista() {
   this.espacioListas = document.getElementById("espacioListas");
   this.crearTitulo = document.createElement("h2");
   this.tituloLista = document.getElementById("listaNueva").value;
@@ -12,8 +12,8 @@ function crearLista() {
   this.boton = function() {
     var boton = document.createElement("BUTTON");
     boton.setAttribute("type", "button");
-    boton.setAttribute("onclick", "crearPendiente();");
-    boton.setAttribute("id", "boton");
+    boton.setAttribute("onclick", "Pendiente();");
+    boton.setAttribute("id", "botoncito");
     var textoBoton = document.createTextNode("Crear pendiente");
     boton.appendChild(textoBoton);
     espacioListas.appendChild(boton);
@@ -23,18 +23,22 @@ function crearLista() {
   document.getElementById("listaNueva").value = "";
   lista();//ambas funciones debo invocarlas aqui para que me cree
   boton();//lo necesario al mismo tiempo que se crea la lista
-  crearPendiente();
 }
 
-function crearPendiente() {
-  var lista = document.createElement("ul");
-  var pendiente = document.createElement("li");
-  var entradaPendiente = document.getElementById("entradaPendiente").value;
-  var boton2 = entradaPendiente;
-  //siguienteEntradaPendiente.id = Date.now();
+function Pendiente() {
+  this.lista = document.createElement("ul");
+  this.pendiente = document.createElement("li");
+  this.entradaPendiente = document.getElementById("entradaPendiente").value;
+  this.nuevaEntrada = function() {
+    entradaPendiente.id = Date.now();
+  }
+
   lista.appendChild(pendiente);
   pendiente.innerText = entradaPendiente;
   document.getElementById("entradaPendiente").value = "";
   espacioListas.appendChild(lista);
-
 }
+/*Averiguar:
+  +porque solo me sirve el primer input
+  +agregar botones que borren
+*/
