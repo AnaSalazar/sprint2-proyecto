bd2a94cfb69507a3b575171b50a5732fdae59813
AnaSalazar <anastephany.salazarguzman@gmail.com>
2017-03-29 11:56:37 -0600

    Agregando funcion crearPendiente(); pero tiene fallas

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,38 @@ function crearLista() {
   this.crearTitulo = document.createElement("h2");
   this.tituloLista = document.getElementById("listaNueva").value;
   this.lista = function(){
-    var pendiente = document.createElement("INPUT");
-    pendiente.setAttribute("type", "text");
-    pendiente.setAttribute("placeholder", "Escribe tu pendiente");
-    espacioListas.appendChild(pendiente);
+    var entradaPendiente = document.createElement("INPUT");
+    entradaPendiente.setAttribute("type", "text");//con esto le pongo los atributos que necesito
+    entradaPendiente.setAttribute("placeholder", "Escribe tu pendiente");
+    entradaPendiente.setAttribute("id", "entradaPendiente");
+    espacioListas.appendChild(entradaPendiente);
   }
-
-
+  this.boton = function() {
+    var boton = document.createElement("BUTTON");
+    boton.setAttribute("type", "button");
+    boton.setAttribute("onclick", "crearPendiente();");
+    boton.setAttribute("id", "boton");
+    var textoBoton = document.createTextNode("Crear pendiente");
+    boton.appendChild(textoBoton);
+    espacioListas.appendChild(boton);
+  }
   crearTitulo.innerText = tituloLista;
   espacioListas.appendChild(crearTitulo);
-  lista();
+  document.getElementById("listaNueva").value = "";
+  lista();//ambas funciones debo invocarlas aqui para que me cree
+  boton();//lo necesario al mismo tiempo que se crea la lista
+  crearPendiente();
+}
+
+function crearPendiente() {
+  var lista = document.createElement("ul");
+  var pendiente = document.createElement("li");
+  var entradaPendiente = document.getElementById("entradaPendiente").value;
+  var boton2 = entradaPendiente;
+  //siguienteEntradaPendiente.id = Date.now();
+  lista.appendChild(pendiente);
+  pendiente.innerText = entradaPendiente;
+  document.getElementById("entradaPendiente").value = "";
+  espacioListas.appendChild(lista);
+
 }
